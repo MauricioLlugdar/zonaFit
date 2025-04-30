c5c6dc3c153097cb1271abcef743c4ec825a6b67
Maurillugdar <maurillugdar@gmail.com>
2025-04-30 01:53:43 -0300

    FEAT: Create interface for the ClientDao to comunicate w the database

--- src/main/java/zona_fit/data/IClientDao.java
+++ src/main/java/zona_fit/data/IClientDao.java
@@ -1,4 +1,12 @@
 package zona_fit.data;
 
+import zona_fit.domain.Client;
+import java.util.List;
+
 public interface IClientDao {
+    List<Client> showClients();
+    boolean searchClientById(Client client);
+    boolean addClient(Client client);
+    boolean modifyClient(Client client);
+    boolean deleteClient(Client client);
 }
